8e2714c03fc812e8de40c3b6c5553ee414d1d1fa
Bolzano17 <anas.ibrahim@epfl.ch>
2020-02-25 17:19:53 +0100

    test

--- app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -1,19 +1,29 @@
 package ch.epfl.sdp;
 
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
 
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
 @RunWith(AndroidJUnit4.class)
+
 public class MainActivityTest {
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule =
