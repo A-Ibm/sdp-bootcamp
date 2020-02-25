a3f960c5ac69fb0b3dab52c0c1857f307c8e44d8
Bolzano17 <anas.ibrahim@epfl.ch>
2020-02-25 17:40:30 +0100

    travis

--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// frfr
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
